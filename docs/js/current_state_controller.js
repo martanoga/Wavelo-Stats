07ee8b45c94e8ee7509d8372ebf82cf417ca6ac0
Marta Noga <marta.noga@gmail.com>
2017-04-16 00:05:35 +0200

    added infoWindow

--- docs/js/current_state_controller.js
+++ docs/js/current_state_controller.js
@@ -1,12 +1,26 @@
 angular.module('wavelo.stats.currentState', ['wavelo.stats.bikesDataService',
-// 'uiGmapgoogle-maps',
-'ngMap'
+    // 'uiGmapgoogle-maps',
+    'ngMap',
+
 ])
     .controller('currentStateCtrl', function ($scope, $http, $interval, BikesData, NgMap) {
-        $scope.map = { center: { latitude: 50.0569018, longitude: 19.9247884}, zoom: 13 };
+        $scope.map_data = { center: { latitude: 50.0569018, longitude: 19.9247884 }, zoom: 13 };
+        NgMap.getMap().then(function (map) {
+            console.log('map', map);
+            $scope.map = map;
+        });
+
         BikesData.getCurrentState()
-            .then(function(data){
+            .then(function (data) {
                 for (var date in data)
                     $scope.current_data = data[date]['unavailable_bikes'];
-            })  
+            })
+        $scope.showDetail = function (event, bike) {
+            $scope.bike = bike;
+            $scope.map.showInfoWindow('foo-iw',"m" + bike.id);
+            
+        };
+        $scope.hideDetail = function () {
+            $scope.map.hideInfoWindow('foo-iw');
+        };
     })
